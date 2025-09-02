86ce7ccd3eacf931e816efddacdbff2b7c230002
Hojoon Kim <hojoon0724@gmail.com>
2025-09-01 19:29:38 -0700

    added favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
-
 import { Footer } from "@/components/organisms";
 
-
-import SideNavBar from "@/components/organisms/SideNavBar";
+import { SideNavBar } from "@/components/organisms";
 import type { Metadata } from "next";
+import HojoonKimFooter from "../components/atoms/HojoonKim";
 import "../styles/globals.css";
-import HojoonKimFooter from '../components/atoms/HojoonKim';
 
 export const metadata: Metadata = {
   title: "ensembleNewSRQ",
   description: "ensembleNewSRQ",
+  icons: {
+    icon: "/favicon.ico", // or your preferred favicon file
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
@@ -23,7 +25,7 @@ export default function RootLayout({
         <SideNavBar />
         <main className="flex-1 lg:ml-20 w-screen lg:w-[calc(100svw-80px)] h-full">{children}</main>
         <Footer className="lg:ml-20 lg:max-w-[calc(100svw-80px)]" />
-        <HojoonKimFooter className="lg:ml-20 lg:max-w-[calc(100svw-80px)]"/>
+        <HojoonKimFooter className="lg:ml-20 lg:max-w-[calc(100svw-80px)]" />
       </body>
     </html>
   );
