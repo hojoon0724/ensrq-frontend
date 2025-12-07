84227432f6d2e17de9d540c7634ccc2802cc0ef0
Hojoon Kim <hojoon@Hojoons-MacBook-Pro.local>
2025-12-06 19:10:37 -0800

    added meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,30 @@ export default function RootLayout({
             gtag('config', 'G-9BB8LKLHT0');
           `}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '157098818575376');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=157098818575376&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body className="flex flex-col w-screen min-h-screen items-center justify-between">
         <SideNavBar />
